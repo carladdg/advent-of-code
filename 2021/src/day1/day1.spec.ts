d2f819bc4d862bef7b64b3a84cae047936665e80
Carla Garcia <cdgarcia95@gmail.com>
2021-12-27 08:55:52 -0700

    Install eslint and prettier

--- 2021/src/day1/day1.spec.ts
+++ 2021/src/day1/day1.spec.ts
@@ -1,13 +1,13 @@
-import {part1, part2, parseInput} from "./day1";
+import { part1, part2, parseInput } from './day1'
 
-describe("day1", () => {
-    const input = parseInput("./files/day1-test.txt");
+describe('day1', () => {
+  const input = parseInput('./files/day1-test.txt')
 
-    it("counts the number of individual depth measurement increases", () => {
-        expect(part1(input)).toEqual(7);
-    });
+  it('counts the number of individual depth measurement increases', () => {
+    expect(part1(input)).toEqual(7)
+  })
 
-    it("counts the number of increases across 3-day sliding windows", () => {
-        expect(part2(input)).toEqual(5);
-    })
+  it('counts the number of increases across 3-day sliding windows', () => {
+    expect(part2(input)).toEqual(5)
+  })
 })
--- 2021/src/day1/day1.ts
+++ 2021/src/day1/day1.ts
@@ -1,40 +1,42 @@
-import {readFileSync} from "fs";
+import { readFileSync } from 'fs'
 
-export function part1(input: number[]): number {    
-    return countIncreasingValues(input);
+export function part1(input: number[]): number {
+  return countIncreasingValues(input)
 }
 
 export function part2(input: number[]): number {
-    return countIncreasingValues(convertToSlidingWindows(input));
+  return countIncreasingValues(convertToSlidingWindows(input))
 }
 
 export function parseInput(inputFile: string): number[] {
-    return readFileSync(inputFile, "utf-8").split("\n").map(e => parseInt(e));
+  return readFileSync(inputFile, 'utf-8')
+    .split('\n')
+    .map((e) => parseInt(e))
 }
 
 function countIncreasingValues(values: number[]) {
-    let count = 0;
-    let prev = values[0];
-    for (let i = 1; i < values.length; i++) {
-        const next = values[i];
-        if (next > prev) {
-            count++;
-        }
-        prev = next;
-    };
+  let count = 0
+  let prev = values[0]
+  for (let i = 1; i < values.length; i++) {
+    const next = values[i]
+    if (next > prev) {
+      count++
+    }
+    prev = next
+  }
 
-    return count;
+  return count
 }
 
 function convertToSlidingWindows(input: number[]): number[] {
-    return input.reduce((windows: number[], _, i) => {
-        if (i + 2 < input.length) {
-            windows.push(input[i] + input[i+1] + input[i+2])
-        }
-        return windows;
-    }, []);
+  return input.reduce((windows: number[], _, i) => {
+    if (i + 2 < input.length) {
+      windows.push(input[i] + input[i + 1] + input[i + 2])
+    }
+    return windows
+  }, [])
 }
 
-const input = parseInput("./files/day1.txt");
-console.log(`Part 1: ${part1(input)}`);
-console.log(`Part 2: ${part2(input)}`);
+const input = parseInput('./files/day1.txt')
+console.log(`Part 1: ${part1(input)}`)
+console.log(`Part 2: ${part2(input)}`)
